7d757b7c45b02712611a9482ef6f88fb8c6311f7
barthaleventegabor <barthaleventegabor@ktch.hu>
2025-11-03 08:57:30 +0100

    Akosnak

---
+++ database/migrations/2025_11_03_073802_add_roles_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer("role")->after("password");
+            $table->integer("counter")->after("role");
+            $table->integer("status")->after("counter");
+            $table->timestamp("banning")->after("status")->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
